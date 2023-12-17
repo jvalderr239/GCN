0f4bfe5177512e6819380a2552095b65576aa04c
Jose Valderrama <jvalderr@gatech.edu>
2023-12-17 00:17:08 -0700

    collate dataloader

--- scripts/train.py
+++ scripts/train.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 import torch
 from fire import Fire
+from torch.utils.data.dataloader import default_collate
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -41,6 +42,7 @@ def train(
         root_dir=root_dir,
         obs_len=trainer.seq_len,
         pred_len=trainer.pred_seq_len,
+        collate_fn=lambda x: tuple(x_.to(device) for x_ in default_collate(x))
     )
     log.info("Loading validation set")
     loader_val = train_utils.generate_dataloader(
@@ -51,6 +53,7 @@ def train(
         root_dir=root_dir,
         obs_len=trainer.seq_len,
         pred_len=trainer.pred_seq_len,
+        collate_fn=lambda x: tuple(x_.to(device) for x_ in default_collate(x))
     )
     log.info("Loading test set")
     loader_val = train_utils.generate_dataloader(
