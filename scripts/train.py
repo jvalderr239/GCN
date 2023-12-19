fix: vanishing gradients

scripts/train.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import logging.config
 from datetime import datetime
@@ -25,12 +26,14 @@ def train(
     dest_dir: str,
     root_dir: Optional[str] = None,
     batch_size: int = 1,
-    early_stop_thresh: int = 100,
+    early_stop_thresh: int = 75,
+    checkpoint_path: Optional[str] = None,
     **kwargs,
 ):
     # Defining the model
     if root_dir is None:
         root_dir = str(get_project_root() / "resources") + "/"
+
     trainer = train_utils.Trainer(**kwargs)
     log.info(f"Working with {device} for training")
     log.info("Loading training set")
@@ -84,6 +87,9 @@ def train(
         blocks_to_retrain=trainer.blocks_to_retrain,
     ).to(device)
 
+    if checkpoint_path:
+        stgcnn_model.load_state_dict(torch.load(checkpoint_path))
+
     # Training settings
     optimizer = torch.optim.Adam(
         stgcnn_model.parameters(),
@@ -93,11 +99,12 @@ def train(
         weight_decay=0,
         amsgrad=False,
     )
+
     scheduler = trainer.get_scheduler(optimizer=optimizer)
 
     # Initializing in a separate cell so we can easily add more epochs to the same run
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    writer = SummaryWriter(f"{dest_dir}/runs/social_collision_stgcnn_{timestamp}")
+    writer = SummaryWriter(f"{dest_dir}/runs/{str(stgcnn_model).lower()}_{timestamp}")
 
     best_vloss = 1_000_000.0
 
@@ -132,9 +139,12 @@ def train(
         log.info(f"Training Loss: {avg_loss} Validation Loss: {avg_vloss}")
 
         before_lr = optimizer.param_groups[0]["lr"]
-        scheduler.step()
+        scheduler.step(avg_vloss)
         after_lr = optimizer.param_groups[0]["lr"]
-        log.info(f"Epoch {epoch_number + 1}: SGD lr {before_lr:.4} -> {after_lr:.4}")
+        if before_lr != after_lr:
+            log.info(
+                f"Epoch {epoch_number + 1}: SGD lr {before_lr:.4} -> {after_lr:.4}"
+            )
 
         # Log the running loss averaged per batch
         # for both training and validation
@@ -164,26 +174,39 @@ def train(
         if avg_vloss < best_vloss:
             best_vloss = avg_vloss
             best_epoch = epoch_number + 1
-            model_path = f"{dest_dir}/{stgcnn_model.name.lower()}_{timestamp}_{epoch_number + 1}.pt"
+            model_path = f"{dest_dir}/{str(stgcnn_model).lower()}_{timestamp}_{epoch_number + 1}.pt"
             train_utils.checkpoint(stgcnn_model, model_path)
         elif (epoch_number + 1) - best_epoch > early_stop_thresh:
             log.warning(f"Early stopped training at epoch {epoch_number + 1}")
             break  # terminate the training loop
 
     log.info("Validating against test set")
-    ade, fde, avg_tloss, avg_te_acc, avg_tn_acc, avg_tt_acc = train_utils.test(
+    test_metrics, tracking_test_data = train_utils.test(
         model=stgcnn_model,
         test_loader=loader_test,
         device=device,
     )
+    acc = test_metrics["accuracy"]
+    time_acc = acc["time_of_event"]
+    node_acc = acc["node_index"]
+    event_acc = acc["event_type"]
+    log.info(f"Time Prediction Test MSE: {time_acc}")
+    log.info(f"Event Type Test Accuracy: {event_acc}")
+    log.info(f"Node Index Test Accuracy: {node_acc}")
 
-    log.info(f"Time Prediction Test MSE: {avg_tt_acc}")
-    log.info(f"Event Type Test Accuracy: {avg_te_acc}")
-    log.info(f"Node Index Test Accuracy: {avg_tn_acc}")
-
-    log.info(f"Test Loss: {avg_tloss}")
+    tloss = test_metrics["avg_test_loss"]
+    ade = test_metrics["avg_ade"]
+    fde = test_metrics["avg_fde"]
+    log.info(f"Test Loss: {tloss}")
     log.info(f"ADE: {ade}, FDE: {fde}")
-    log.info("Finished Training...")
+    log.info("Finished Training... Storing results...")
+
+    with open(f"{dest_dir}/test_metrics.json", "w") as fp:
+        json.dump(test_metrics, fp)
+    with open(f"{dest_dir}/test_metrics.json", "w") as fp:
+        json.dump(tracking_test_data, fp)
+
+    log.info(f"Stored results in {dest_dir}")
 
 
 if __name__ == "__main__":

src/models/cnn.py
@@ -55,6 +55,9 @@ class EVENT_PREDICTOR_CNN(nn.Module):
         self.drop = nn.Dropout(p=dropout)
         self.relu = nn.ReLU() if not leaky else nn.LeakyReLU()
 
+    def __str__(self):
+        return "convolutional_predictor"
+
     def build_conv_layer(
         self,
         in_c,
@@ -98,7 +101,7 @@ class PRETRAINED_EVENT_PREDICTOR_CNN(nn.Module):
         num_nodes: int = 22,
         kernel_size: int = 3,
         dropout: float = 0.3,
-        blocks_to_retrain: int = 5,
+        blocks_to_retrain: float = 0.5,
     ):
         super().__init__()
         self.cnn, cnn_output_dim = self._get_base_model(
@@ -124,13 +127,16 @@ class PRETRAINED_EVENT_PREDICTOR_CNN(nn.Module):
             nn.LeakyReLU(),
         )
 
+    def __str__(self):
+        return f"{self._model_name}_predictor"
+
     def _get_base_model(
         self,
         name: str,
         pretrained: bool,
         in_channels: int,
         kernel_size: int,
-        blocks_to_retrain: int,
+        blocks_to_retrain: float,
     ):
         """
         Generate base model and reformat with convolutional layer to reshape as
@@ -154,15 +160,16 @@ class PRETRAINED_EVENT_PREDICTOR_CNN(nn.Module):
             raise ValueError("Currently, there is only support for ResNet backbones...")
 
         selected_model: nn.Module = getattr(models, name.lower())(pretrained=pretrained)
-        self.name: str = f"{name.capitalize()}_PREDICTOR"
+        self._model_name: str = f"{name.capitalize()}_PREDICTOR"
         # Fine-tune pretrained model
         if pretrained:
             num_params = len(list(selected_model.parameters()))
+            num_blocks_to_retrain = int(blocks_to_retrain * num_params)
             log.debug(
-                f"Loaded model has {num_params}... Unfreezing {blocks_to_retrain}"
+                f"Loaded model has {num_params}... Unfreezing {num_blocks_to_retrain}"
             )
             for i, (pname, param) in enumerate(selected_model.named_parameters()):
-                if i + 1 >= (num_params - blocks_to_retrain):
+                if i + 1 >= (num_params - num_blocks_to_retrain):
                     log.debug(f"Unfreezing {pname}")
                     param.requires_grad = True
                 else:

src/models/social_collision_stgcnn.py
@@ -48,7 +48,7 @@ class SOCIAL_COLLISION_STGCNN(nn.Module):
         *,
         cnn: Optional[str] = None,
         pretrained: bool = True,
-        blocks_to_retrain: int = 15,
+        blocks_to_retrain: float = 0.5,
         cnn_dropout: float = 0.3,
         **kwargs,
     ):
@@ -76,7 +76,7 @@ class SOCIAL_COLLISION_STGCNN(nn.Module):
         for _ in range(self.n_txpcnn):
             self.prelus.append(nn.PReLU())
 
-        self.cnn = (
+        self._cnn = (
             PRETRAINED_EVENT_PREDICTOR_CNN(
                 in_channels=input_cnn_feat,
                 name=cnn,
@@ -94,10 +94,10 @@ class SOCIAL_COLLISION_STGCNN(nn.Module):
                 dropout=cnn_dropout,
             )
         )
-        log.info(
-            f"Built {self.cnn.name.lower()} model for prediction"  # pylint: disable=union-attr, operator
-        )
-        self.name = f"SOCIAL_COLLISON_STGCNN_{self.cnn.name}"  # pylint: disable=union-attr, operator
+        log.info(f"Built {str(self._cnn).lower()} model for prediction")
+
+    def __str__(self):
+        return f"SOCIAL_COLLISON_STGCNN_{str(self._cnn)}"
 
     def forward(self, v, a):
         """
@@ -111,7 +111,7 @@ class SOCIAL_COLLISION_STGCNN(nn.Module):
         # Use feature extractor to predict tackle, tackler and time of attack
         # Use the spatial node output along with categorical features
         cnn_input = torch.concat((v, v_original[:, self.num_spatial_nodes :]), dim=1)
-        simo = self.cnn(cnn_input)
+        simo = self._cnn(cnn_input)
         v = v.view(v.shape[0], v.shape[2], v.shape[1], v.shape[3])
 
         v = self.prelus[0](self.tpcnns[0](v))

src/train_utils.py
@@ -1,6 +1,7 @@
 import dataclasses
 import logging
 import logging.config
+from collections import defaultdict
 from typing import Any, Dict, Optional, Tuple
 
 import numpy as np
@@ -35,8 +36,8 @@ class Trainer:
     cnn_name: Optional[str] = "efficientnet_b0"
     pretrained: bool = True
     cnn_dropout: float = 0.3
-    blocks_to_retrain: int = 99
-    clip: Optional[float] = None
+    blocks_to_retrain: float = 0.5
+    clip: Optional[float] = 1.0
 
     # Dataset parameters
     num_features = 21
@@ -44,13 +45,13 @@ class Trainer:
     num_nodes = 22
 
     # Train parameters
-    lr: float = 0.05
+    lr: float = 0.01
 
     def update(self, arg, value):
         setattr(self, arg, value)
 
     def get_scheduler(self, optimizer):
-        return lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+        return lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, factor=0.1)
 
 
 def generate_dataloader(
@@ -228,10 +229,11 @@ def validate(
 
 def test(
     model: nn.Module, test_loader: DataLoader, device: Any, sample_steps: int = 20
-) -> Tuple[float, ...]:
+) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     # Set the model to evaluation mode, disabling dropout and using population
     # statistics for batch normalization.
     model.eval()
+    # Keep track of metrics
     running_tloss = 0.0
     running_time_acc = 0.0
     running_node_acc = 0.0
@@ -239,6 +241,8 @@ def test(
     avg_tloss = 0.0
 
     ade, fde = [], []
+    tracking_data: Dict[str, Any] = defaultdict(dict)
+
     # Disable gradient computation and reduce memory consumption.
     with torch.no_grad():
         for i, batch in enumerate(test_loader):
@@ -276,6 +280,8 @@ def test(
                 (V_tr_x_abs.unsqueeze(-1), V_tr_y_abs.unsqueeze(-1)), dim=-1
             )
 
+            tracking_data[str(i)]["obs"] = V_obs_abs
+            tracking_data[str(i)]["target"] = V_tr_abs
             # Generate deviation data from predictions
             # V_pred includes x, y, sx, sy, corr deviations
             V_pred, simo = infer(model, V_obs=V_obs, A_obs=A_obs, device=device)
@@ -301,6 +307,7 @@ def test(
             )
 
             sample_ade, sample_fde = np.empty((num_nodes, 1)), np.empty((num_nodes, 1))
+            tracking_data[str(i)]["pred"] = []
             for _ in range(sample_steps):
                 V_sample = mvnormal.sample()
 
@@ -313,6 +320,7 @@ def test(
                 V_sample_abs = torch.concat(
                     (V_sample_x_abs.unsqueeze(-1), V_sample_y_abs.unsqueeze(-1)), dim=-1
                 )
+                tracking_data[str(i)]["pred"].append(V_sample_abs)
                 ade_per_node = compute_ade(
                     V_sample_abs.contiguous(), V_tr_abs.contiguous()
                 )
@@ -340,4 +348,14 @@ def test(
     res_ade = float(np.sum(np.array(ade)) / len(ade))
     res_fde = float(np.sum(np.array(fde)) / len(fde))
 
-    return res_ade, res_fde, avg_tloss, avg_te_acc, avg_tn_acc, avg_tt_acc
+    result_metrics = {
+        "avg_test_loss": avg_tloss,
+        "avg_ade": res_ade,
+        "avg_fde": res_fde,
+        "accuracy": {
+            "event_type": avg_te_acc,
+            "node_index": avg_tn_acc,
+            "time_of_event": avg_tt_acc,
+        },
+    }
+    return result_metrics, tracking_data